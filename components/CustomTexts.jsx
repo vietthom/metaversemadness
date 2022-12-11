d1ba763aaf9966af76a02cd34781f81ac7c0237b
vietthom <thomle0418@gmail.com>
2022-12-10 18:10:10 -0700

    Custom text for title

--- components/CustomTexts.jsx
+++ components/CustomTexts.jsx
@@ -8,14 +8,21 @@ export const TypingText = ({ title, textStyles }) => (
     variants={textContainer}
     className={`front-normal text-[14px] text-secondary-white ${textStyles}`}
   >
-    {Array.from(title).map((Letter, index) => (
+    {Array.from(title).map((letter, index) => (
       <motion.span variants={textVariant2} key={index}>
-        {Letter === ' ' ? '\u00A0' : Letter}
+        {letter === ' ' ? '\u00A0' : letter}
       </motion.span>
     ))}
-  </motion.p>  
+  </motion.p>
 );
 
 export const TitleText = ({ title, textStyles }) => (
-  <h2>Title Text</h2>
+  <motion.h2
+    variants={textVariant2}
+    initial="hidden"
+    whileInView="show"
+    className={`mt-[8px] font-bold md:text-[64px] text-[40px] text-white ${textStyles}`}
+  >
+    {title}
+  </motion.h2>
 );
